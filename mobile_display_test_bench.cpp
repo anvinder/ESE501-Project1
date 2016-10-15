21a8ed146c4a2f43b1aefc472affd98cb76f809e
anvinder <anvinder_singh26@yahoo.co.in>
2016-10-14 19:40:30 -0700

    Update mobile_display_test_bench.cpp

--- mobile_display_test_bench.cpp
+++ mobile_display_test_bench.cpp
@@ -3,11 +3,17 @@
 int sc_main(int argc, char* argv[]) {
 	sc_signal < bool > clk;	
 	sc_signal < sc_bigint <8> > x_1, y_1, x_2, y_2;
+	sc_signal < sc_bigint <8> > x_12, y_12, x_22, y_22, x_13, y_13, x_23, y_23;
 	sc_signal < sc_bigint <8> > starttime, endtime;
+	sc_signal < sc_bigint <8> > starttime2, endtime2;
+	sc_signal < sc_bigint <8> > starttime3, endtime3;
 	sc_signal < sc_bigint <1> > waittime;
+	sc_signal < sc_bigint <1> > waittime2, waittime3;
 	sc_signal < sc_bigint<49> > physical_packet_out;
+	sc_signal < sc_bigint<49> > physical_packet_out2;
+	sc_signal < sc_bigint<49> > physical_packet_out3;
 	sc_signal <sc_bigint<1> > copy_new_image_flag;
-	//sc_clock clock("clock", 10, SC_NS, .5);				
+	//sc_clock clock("clock", 80, SC_NS, 1);				
 	//int i;
 
 	mobile_processor m1("mobile_processor");
@@ -15,11 +21,32 @@ int sc_main(int argc, char* argv[]) {
 	m1.y_1(y_1);
 	m1.x_2(x_2);
 	m1.y_2(y_2);
+
+  m1.x_12(x_12);
+	m1.y_12(y_12);
+	m1.x_22(x_22);
+	m1.y_22(y_22);
+
+	m1.x_13(x_13);
+	m1.y_13(y_13);
+	m1.x_23(x_23);
+	m1.y_23(y_23);
+	
 	m1.clk(clk);
 	m1.starttime(starttime);
 	m1.endtime(endtime);
 	m1.waittime(waittime);
+	
+	m1.starttime2(starttime2);
+	m1.endtime2(endtime2);
+	m1.waittime2(waittime2);
+	m1.starttime3(starttime3);
+	m1.endtime3(endtime3);
+	m1.waittime3(waittime3);
+
 	m1.physical_packet_out(physical_packet_out);
+	m1.physical_packet_out2(physical_packet_out2);
+	m1.physical_packet_out3(physical_packet_out3);
 	m1.copy_new_image_flag(copy_new_image_flag);
 
 	//open vcd file
@@ -31,10 +58,31 @@ int sc_main(int argc, char* argv[]) {
 	sc_trace(file, y_1, "y_1");
 	sc_trace(file, x_2, "x_2");
 	sc_trace(file, y_2, "y_2");
+
+	sc_trace(file, x_12, "x_12");
+	sc_trace(file, y_12, "y_12");
+	sc_trace(file, x_22, "x_22");
+	sc_trace(file, y_22, "y_22");
+
+	sc_trace(file, x_13, "x_13");
+	sc_trace(file, y_13, "y_13");
+	sc_trace(file, x_23, "x_23");
+	sc_trace(file, y_23, "y_23");
+
 	sc_trace(file, starttime, "starttime");
 	sc_trace(file, endtime, "endtime");
 	sc_trace(file, waittime, "waittime");
+
+		sc_trace(file, starttime2, "starttime2");
+	sc_trace(file, endtime2, "endtime2");
+	sc_trace(file, waittime2, "waittime2");
+		sc_trace(file, starttime3, "starttime3");
+	sc_trace(file, endtime3, "endtime3");
+	sc_trace(file, waittime3, "waittime3");
+
 	sc_trace(file, physical_packet_out, "physical_packet_out");
+	sc_trace(file, physical_packet_out2, "physical_packet_out2");
+	sc_trace(file, physical_packet_out3, "physical_packet_out3");
 	sc_trace (file, copy_new_image_flag, "copy_new_image_flag");
 
 
@@ -48,7 +96,7 @@ for (int i = 0; i<=80; i++)
 };
 
 
-sc_start(1, SC_NS);
+sc_start(80, SC_NS);
 sc_close_vcd_trace_file(file);
 sc_stop();
 return (0);
